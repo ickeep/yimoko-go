4ba4f364bb51a80e863fd1a89928509542182ad1
keepcai <keepcai@tencent.com>
2023-11-19 11:11:50 +0800

    ToSnake

--- utils/string.go
+++ utils/string.go
@@ -4,6 +4,7 @@ import (
 	"math/rand"
 	"strings"
 	"time"
+	"unicode"
 )
 
 // FirstUpper _
@@ -48,4 +49,27 @@ func RandomStr(n int) string {
 		remain--
 	}
 	return sb.String()
+}
+
+// ToSnake copy 自 ent
+func ToSnake(s string) string {
+	var (
+		j int
+		b strings.Builder
+	)
+	for i := 0; i < len(s); i++ {
+		r := rune(s[i])
+		// Put '_' if it is not a start or end of a word, current letter is uppercase,
+		// and previous is lowercase (cases like: "UserInfo"), or next letter is also
+		// a lowercase and previous letter is not "_".
+		if i > 0 && i < len(s)-1 && unicode.IsUpper(r) {
+			if unicode.IsLower(rune(s[i-1])) ||
+				j != i-1 && unicode.IsLower(rune(s[i+1])) && unicode.IsLetter(rune(s[i-1])) {
+				j = i
+				b.WriteString("_")
+			}
+		}
+		b.WriteRune(unicode.ToLower(r))
+	}
+	return b.String()
 }
